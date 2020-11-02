0fcc617a91b95096e43242ae5a8ea4cc69d9168c
fadzlanilham10 <fadzlanilham10@gmail.com>
2020-11-02 23:34:19 +0700

    praktikum

---
+++ Praktikum/P1/kenalan.php
@@ -0,0 +1,11 @@
+
+<?php
+class kenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "<h2> Fadzlan Ilham<br> </h2>
+                saya tinggal di Pagedangan<br>
+                saya suka volly";
+    }
+
+}
---
+++ pustakafadz/application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fadzlan</li>
+        <li>Nama Belakang : Izzudin</li>
+    </ul> 
+    <br>
+    <h4>Alamat</h4>  
+    <ul type="none">
+        <li>Jln. Raya Pagedangan</li>
+    </ul>
+    <h4>Tempat lahir</h4>  
+    <ul type="none">
+        <li>Tegal</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>  
+    <ul type="square">
+        <li>Volly</li>
+    </ul>
+</section>
